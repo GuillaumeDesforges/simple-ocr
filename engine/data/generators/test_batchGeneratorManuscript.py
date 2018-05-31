9bd4af24bdd66c76b5632f0d60803b60ae6b7c71
Guillaume Desforges <aceus02@gmail.com>
2018-05-31 18:34:23 +0200

    batch generator manuscript first unit tests

--- engine/data/generators/test_batchGeneratorManuscript.py
+++ engine/data/generators/test_batchGeneratorManuscript.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+import numpy as np
+
 from engine.data.generators.batch_generator_manuscript import BatchGeneratorManuscript
 
 
@@ -8,4 +10,19 @@ class TestBatchGeneratorManuscript(TestCase):
         self.generator = BatchGeneratorManuscript('fixtures/manuscript/')
 
     def test_len(self):
-        self.assertEqual(len(self.generator) == 2)
+        self.assertEqual(len(self.generator) == 7)
+
+    def test_getitem(self):
+        item: dict = self.generator.__getitem__(0)
+        keys = item.keys()
+
+        self.assertIn('x' in keys)
+        self.assertIn('y' in keys)
+        self.assertIn('x_widths' in keys)
+        self.assertIn('y_widths' in keys)
+
+        x, x_widths, y, y_widths = item['x'], item['x_widths'], item['y'], item['y_widths']
+        self.assertIs(type(x), np.ndarray)
+        self.assertIs(type(y), np.ndarray)
+        self.assertIs(type(x_widths), int)
+        self.assertIs(type(y_widths), int)
